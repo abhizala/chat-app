a0fa747cf668fed6ee25bc0d6aa8a67cc98ab753
abhizala <abhizala@hotmail.com>
2019-06-25 18:50:27 +0530

    Sharing location change and Event Acknowledgements

--- public/js/chat.js
+++ public/js/chat.js
@@ -9,17 +9,26 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
 
     const message = e.target.elements.message.value
 
-    socket.emit('sendMessage', message)
-})
- document.querySelector('#send-location').addEventListener('click', () => {
-     if (!navigator.geolocation) {
-         return alert('Geolation is not supported by your browser')
-     }
+    socket.emit('sendMessage', message, (error) => {
+        if (error) {
+            return console.log(error)
+        }
 
-     navigator.geolocation.getCurrentPosition((postion) => {
-         socket.emit('sendLocation', {
-             latitude: postion.coords.latitude,
-             longitude: postion.coords.longitude
-         })
-     })
- })
+        console.log('Message delivered!')
+    })
+})
+
+document.querySelector('#send-location').addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser.')
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        }, () => {
+            console.log('Location shared!')
+        })
+    })
+})
